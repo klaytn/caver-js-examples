d1a9a05d4a9f725d07769e6429ea154ac6305fe1
Jasmine <32922423+jimni1222@users.noreply.github.com>
2021-04-05 17:28:38 +0900

    Update kip17_kas_api.js

--- kip17/kip17_kas_api.js
+++ kip17/kip17_kas_api.js
@@ -15,36 +15,36 @@ const caver = new CaverExtKAS(chainId, accessKeyId, secretAccessKey)
 test()
 
 async function test () {
-	const tokenName = 'TokenName'
-	const symbol = 'TN'
-	// Alias should be unique. If send duplicated alias, KAS will return an error.
+    const tokenName = 'TokenName'
+    const symbol = 'TN'
+    // Alias should be unique. If send duplicated alias, KAS will return an error.
     const alias = `kip-17-contract-alias`
-	const deployedResult = await caver.kas.kip17.deploy(tokenName, symbol, alias)
+    const deployedResult = await caver.kas.kip17.deploy(tokenName, symbol, alias)
 
-	console.log(deployedResult)
+    console.log(deployedResult)
 
-	// Wait for KIP-17 that requested deployment to be successfully deployed to Klaytn and applied in KAS KIP-17 API.
-	await timeout(10000)
+    // Wait for KIP-17 that requested deployment to be successfully deployed to Klaytn and applied in KAS KIP-17 API.
+    await timeout(10000)
 
     const tokenId = '1'
-	const uri = 'http://test.url'
+    const uri = 'http://test.url'
 
-	// Create an account in KAS Wallet API
-	const account = await caver.kas.wallet.createAccount()
-	console.log(`Account in KAS Wallet API`)
-	console.log(account)
+    // Create an account in KAS Wallet API
+    const account = await caver.kas.wallet.createAccount()
+    console.log(`Account in KAS Wallet API`)
+    console.log(account)
 
-	// mint(addressOrAlias, to, tokenId, tokenURI
+    // mint(addressOrAlias, to, tokenId, tokenURI
     const mintResult = await caver.kas.kip17.mint(alias, account.address, tokenId, uri)
     console.log(`mint result: `)
     console.log(mintResult)
 
-	// transfer(addressOrAlias, sender, owner, to, tokenId)
-	const transferResult = await caver.kas.kip17.transfer(alias, account.address, account.address, account.address, tokenId)
-	console.log(`transfer result: `)
+    // transfer(addressOrAlias, sender, owner, to, tokenId)
+    const transferResult = await caver.kas.kip17.transfer(alias, account.address, account.address, account.address, tokenId)
+    console.log(`transfer result: `)
     console.log(transferResult)
 
-	// burn(addressOrAlias, from, tokenId)
+    // burn(addressOrAlias, from, tokenId)
     const burnResult = await caver.kas.kip17.burn(alias, account.address, tokenId)
     console.log(`burn result: `)
     console.log(burnResult)
