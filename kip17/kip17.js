6676b3bc64f7f58d72ae85c546588238115e14f4
Jasmine/kimjimin <wlalsdlwwkd@gmail.com>
2021-04-05 15:25:06 +0900

    Added KIP-17 examples

---
+++ kip17/kip17.js
@@ -0,0 +1,54 @@
+const Caver = require('caver-js')
+
+const URL_NODE_API = 'https://node-api.klaytnapi.com/v1/klaytn'
+
+// Configuration Part
+// Set your KAS access key and secretAccessKey.
+const accessKeyId = '{your_accessKeyId}'
+const secretAccessKey = '{your_secretAccessKey}'
+
+// const CHAIN_ID_BAOBOB = '1001'
+// const CHAIN_ID_CYPRESS = '8217'
+const chainId = '1001'
+
+const option = {
+    headers: [
+        { name: 'Authorization', value: 'Basic ' + Buffer.from(accessKeyId + ':' + secretAccessKey).toString('base64') },
+        { name: 'x-chain-id', value: chainId },
+    ]
+}
+
+const caver = new Caver(new Caver.providers.HttpProvider(URL_NODE_API, option))
+
+test()
+
+async function test () {
+    const privateKey = '0x{private key}'
+
+    // Add keyring to in-memory wallet
+    const keyring = caver.wallet.keyring.createFromPrivateKey(privateKey)
+	caver.wallet.add(keyring)
+	
+	// caver.kct.kip17.deploy returns a KIP17 instance
+	const kip17 = await caver.kct.kip17.deploy({
+		name: 'Jasmine',
+		symbol: 'JAS',
+	}, keyring.address)
+
+	const kip17Address = kip17.options.address
+	console.log(`kip17Address : ${kip17Address}`)
+
+	const tokenId = '1'
+	const uri = 'http://test.url'
+	const mintReceipt = await kip17.mintWithTokenURI(keyring.address, tokenId, uri, { from:keyring.address })
+	console.log(`mint receipt: `)
+	console.log(mintReceipt)
+
+	const transferReceipt = await kip17.transferFrom(keyring.address, keyring.address, tokenId, {from:keyring.address})
+	console.log(`transfer receipt: `)
+	console.log(transferReceipt)
+
+	const burnReceipt = await kip17.burn(tokenId, {from:keyring.address})
+	console.log(`burn receipt: `)
+	console.log(burnReceipt)
+}
---
+++ kip17/kip17_with_kas_sdk.js
@@ -0,0 +1,49 @@
+const CaverExtKAS = require('caver-js-ext-kas')
+
+// Configuration Part
+// Set your KAS access key and secretAccessKey.
+const accessKeyId = '{your_accessKeyId}'
+const secretAccessKey = '{your_secretAccessKey}'
+
+// const CHAIN_ID_BAOBOB = '1001'
+// const CHAIN_ID_CYPRESS = '8217'
+const chainId = '1001'
+
+const caver = new CaverExtKAS(chainId, accessKeyId, secretAccessKey)
+
+test()
+
+async function test () {
+    const privateKey = '0x{private key}'
+
+	const keyringContainer = new caver.keyringContainer()
+
+	// Add keyring to in-memory wallet
+    const keyring = keyringContainer.keyring.createFromPrivateKey(privateKey)
+	keyringContainer.add(keyring)
+	
+	// caver.kct.kip17.deploy returns a KIP17 instance
+	// If you send IWallet as a last paramter, caver.kct.kip17 will use that to sign.
+	// If you create a KIP17 instance, you can call `kip17.setWallet(keyringContainer)` to use KeyringContainer instead of KAS Wallet API
+	const kip17 = await caver.kct.kip17.deploy({
+		name: 'Jasmine',
+		symbol: 'JAS',
+	}, keyring.address, keyringContainer)
+
+	const kip17Address = kip17.options.address
+	console.log(`kip17Address : ${kip17Address}`)
+
+	const tokenId = '1'
+	const uri = 'http://test.url'
+	const mintReceipt = await kip17.mintWithTokenURI(keyring.address, tokenId, uri, { from:keyring.address })
+	console.log(`mint receipt: `)
+	console.log(mintReceipt)
+
+	const transferReceipt = await kip17.transferFrom(keyring.address, keyring.address, tokenId, {from:keyring.address})
+	console.log(`transfer receipt: `)
+	console.log(transferReceipt)
+
+	const burnReceipt = await kip17.burn(tokenId, {from:keyring.address})
+	console.log(`burn receipt: `)
+	console.log(burnReceipt)
+}
