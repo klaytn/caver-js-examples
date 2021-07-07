48e2cf79b406f93290eeaf2709063d68ff4941c8
Denver(Hyunwoo Lee) <aeharvlee@gmail.com>
2021-07-07 16:02:21 +0900

    Apply suggestions from code review
    Co-authored-by: Jasmine <32922423+jimni1222@users.noreply.github.com>

--- rpc/klay_get_block_receipts/boilerplate.js
+++ rpc/klay_get_block_receipts/boilerplate.js
@@ -4,12 +4,12 @@ const Caver = require('caver-js')
 const ROOT_DIR = path.join(__dirname, '../..'); // Path can be changed based on its actual location.
 
 // You can directly input values for the variables below, or you can enter values in the caver-js-examples/.env file.
-let nodeApiUrl = ""; // e.g. "https://node-api.klaytnapi.com/v1/klaytn";
-let accessKeyId = ""; // e.g. "KASK1LVNO498YT6KJQFUPY8S";
-let secretAccessKey = ""; // e.g. "aP/reVYHXqjw3EtQrMuJP4A3/hOb69TjnBT3ePKG";
-let chainId = ""; // e.g. "1001" or "8217";
-let senderAddress = ""; // e.g. "0xeb709d59954f4cdc6b6f3bfcd8d531887b7bd199"
-let senderPrivateKey = ""; // e.g. "0x39a6375b608c2572fadb2ed9fd78c5c456ca3aa860c43192ad910c3269727fc1"
+let nodeApiUrl = '' // e.g. 'https://node-api.klaytnapi.com/v1/klaytn'
+let accessKeyId = '' // e.g. 'KASK1LVNO498YT6KJQFUPY8S'
+let secretAccessKey = '' // e.g. 'aP/reVYHXqjw3EtQrMuJP4A3/hOb69TjnBT3ePKG'
+let chainId = '' // e.g. '1001' or '8217'
+let senderAddress = '' // e.g. '0xeb709d59954f4cdc6b6f3bfcd8d531887b7bd199'
+let senderPrivateKey = '' // e.g. '0x39a6375b608c2572fadb2ed9fd78c5c456ca3aa860c43192ad910c3269727fc1'
 
 /**
  * Boilerplate code about "How to get receipts included in a block."
@@ -19,7 +19,7 @@ let senderPrivateKey = ""; // e.g. "0x39a6375b608c2572fadb2ed9fd78c5c456ca3aa860
 async function main() {
     try {
         loadEnv()
-        run()
+        await run()
     } catch (err) {
         console.error(err)
     }
@@ -28,17 +28,17 @@ async function main() {
 main()
 
 function loadEnv() {
-    result = dotenv.config({path: `${ROOT_DIR}/.env`})
+    envs = dotenv.config({ path: `${ROOT_DIR}/.env` })
     if (result.error) {
         throw result.error
     }
 
-    nodeApiUrl = nodeApiUrl === "" ? result.parsed.NODE_API_URL : nodeApiUrl
-    accessKeyId = accessKeyId === "" ? result.parsed.ACCESS_KEY_ID : accessKeyId
-    secretAccessKey = secretAccessKey === "" ? result.parsed.SECRET_ACCESS_KEY : secretAccessKey
-    chainId = chainId === "" ? result.parsed.CHAIN_ID : chainId
-    senderAddress = senderAddress === "" ? result.parsed.SENDER_ADDRESS : senderAddress
-    senderPrivateKey = senderPrivateKey === "" ? result.parsed.SENDER_PRIVATE_KEY : senderPrivateKey
+    nodeApiUrl = nodeApiUrl === '' ? envs.parsed.NODE_API_URL : nodeApiUrl
+    accessKeyId = accessKeyId === '' ? envs.parsed.ACCESS_KEY_ID : accessKeyId
+    secretAccessKey = secretAccessKey === '' ? envs.parsed.SECRET_ACCESS_KEY : secretAccessKey
+    chainId = chainId === '' ? envs.parsed.CHAIN_ID : chainId
+    senderAddress = senderAddress === '' ? envs.parsed.SENDER_ADDRESS : senderAddress
+    senderPrivateKey = senderPrivateKey === '' ? envs.parsed.SENDER_PRIVATE_KEY : senderPrivateKey
 }
 
 async function run() {
@@ -48,7 +48,7 @@ async function run() {
                 name: 'Authorization',
                 value: 'Basic ' + Buffer.from(accessKeyId + ':' + secretAccessKey).toString('base64')
             },
-            {name: 'x-chain-id', value: chainId},
+            { name: 'x-chain-id', value: chainId },
         ]
     }
     const caver = new Caver(new Caver.providers.HttpProvider(nodeApiUrl, option))
